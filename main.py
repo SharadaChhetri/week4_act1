8ce785ff5873832939a1bf17f2af0711fff42f8d
SharadaChhetri <csharada551@gmail.com>
2025-05-27 15:19:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import os
 
 def main():
     api_key = os.getenv("API_KEY")
+    print(f"Calling external API with key: {api_key[:4]}*** (hidden)")
     if not api_key:
         print("No API key provided!")
         return
-
     # Simulate calling an external API
-    print(f"Calling external API with key: {api_key[:4]}*** (hidden)")
 
 if __name__ == "_main_":
     main()
